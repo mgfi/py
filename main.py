0bdf1612882794b05d0c42b0adf8c80871cb4bb2
Grzegorz Mikina <gregorimikina@gmail.com>
2024-10-05 23:20:04 +0200

    Data types

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Kolejność:")
+print("Dzielenie:")
+print(5/2)
+# Dzielenie bez reszty
+print(5//2) 
+print("Mnożenie:")
+print(5*2) 
+# Potęgowanie
+print("Skrócone:")
+x = 5
+x = x + 1
+print(x)
+x += 10
+print(x)
+
+# Konwersja Typów 
+print("Konwersja:")
+a = input("Podaj liczbę 1: ")
+b = input("Podaj liczbę 2: ")
+print(a+b)
+
+# Konwersja na integer
+print(int(a)+int(b))
+
+
